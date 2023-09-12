022377623abe9d7a6eab2200c1904b4a7ecb8b4f
paccach <andreypugach228@gmail.com>
2023-09-12 17:02:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+print('ax^2 + bx + c')
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+D = b * b - (4 * a * c)
+if D < 0:
+    print('уравнение не имеет корней')
+elif D == 0:
+    x = -b / (2 * a)
+    print('уравнение имеет один корень: ', x)
+else:
+    x1 = (-b + sqrt(D)) / (2 * a)
+    x2 = (-b - sqrt(D)) / (2 * a)
+    print('уравнение имеет два корня: ', x1, ' ', x2)
